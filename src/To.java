c31ef7489cdd7ca37fec7349a0b14910b0a7b1f8
Tropical <up201306506@gcloud.fe.up.pt>
2016-06-04 19:01:11 +0100

    :tangerine: Implementei o PASS

--- src/To.java
+++ src/To.java
@@ -31,49 +31,42 @@ public class To extends Path{
 		}
 		holder += "\n";
 
-		// Cypher Translation		
+		//-------------------
+		// Cypher Translation
+		//-------------------
+		holder += "---------------------\n";
+		
+		//Define the Path
 		String pathString = String.format(
 				"MATCH  p=(src:Place { name:'%s'})-[:ROAD*1..100]->(dest:Place { name:'%s'})\n", 
 				sourceNodeName, destNodeName);
 		holder += pathString;
-		
+		//Define restriction nodes
 		if(usePassingRestriction){
 			for(String s : restrictionPathNodes)
 				holder += ",(node"+s+":Place { name:'"+s+"' })\n";
 		}
-		
+		//Prevent Backtracking
 		holder += "WHERE ALL(n in nodes(p) where 1=size(filter(m in nodes(p) WHERE m=n)))";
-		
+		//Define Restriction Type
 		if(usePassingRestriction){
-			if(!toPass)
+			if(toPass)
+				for(String s : restrictionPathNodes)
+					holder +=  "AND node"+s+" IN nodes(p)";
+			else
 				for(String s : restrictionPathNodes)
 					holder +=  "AND NONE (n IN nodes(p) WHERE n=node"+s+")\n";
+				
 		}
-		
+		//Define min/max
 		holder += "RETURN p AS path,\n";
 		holder += "reduce(" + attributeFocus + "=0, r in relationships(p) | "
 				+ attributeFocus + "+r." + attributeFocus + ") AS total" + attributeFocus + "\n";
 		holder += "ORDER BY total" + attributeFocus + " ";
 		if(toMinimize) holder += "ASC\n";
 		else holder += "DESC\n";
-		
+		//Number of paths returned
 		holder += "LIMIT 1";
-
-		/*
-		//Original code block
-
-		holder += sourceNodeName + "#" + destNodeName + "%";
-		if(toMinimize) holder += "minimize";
-		else holder += "maximize";
-		holder += "-" + attributeFocus + "%";
-		if(usePassingRestriction){
-			if(toPass) holder += "Pass->";
-			else holder += "NotPass->";
-
-			for(String s : restrictionPathNodes)
-				holder += s + ">";
-		}
-		 */
 
 		return holder;
 	}
