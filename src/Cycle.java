d0ae29488ab2d7236b5b2c23ae571c43dd0f7f2c
Tropical <up201306506@gcloud.fe.up.pt>
2016-06-05 05:41:44 +0100

    :tropical_drink: 3/3 Completo o Cycle e SimpleCycle todo

--- src/Cycle.java
+++ src/Cycle.java
@@ -39,13 +39,32 @@ public class Cycle extends Path {
 			//Set pattern as a Cycle
 			holder += "MATCH path = (src:Place { name:'"
 			+sourceNodeName+ "'})-[:ROAD*1..50]-(src:Place { name:'"+sourceNodeName+"'})\n";
+			//Define restriction nodes
+			if(usePassingRestriction){
+				for(String s : restrictionPathNodes)
+					holder += ",(node"+s+":Place { name:'"+s+"' })\n";
+			}
 			//Choose variables to use
 			holder += "WITH path, src,reduce(" +attributeFocus + "=0, r in relationships(path) | "
 				+ attributeFocus + "+r." + attributeFocus + ") AS total" + attributeFocus + "\n";
+			if(usePassingRestriction){
+				for(String s : restrictionPathNodes)
+					holder += ",node"+s+"\n";
+			}
 			//Force single occurrence of every in-path node for ONLY THE SOURCE nodes
 			holder += "WHERE SINGLE(y IN tail(nodes(path)) WHERE src=y)\n";
 			//Force minimum value of focused variable
 			holder += "AND total"+attributeFocus+" >= "+minLength+"\n";
+			//Define Restriction Type
+			if(usePassingRestriction){
+				if(toPass)
+					for(String s : restrictionPathNodes)
+						holder +=  "AND node"+s+" IN nodes(path)\n";
+				else
+					for(String s : restrictionPathNodes)
+						holder +=  "AND NONE (n IN nodes(path) WHERE n=node"+s+")\n";
+					
+			}
 			// Return values
 			holder += "RETURN path, total"+attributeFocus+"\n";
 			//Order FIRST by reduction of variable, THEN largest path (in nodes)
@@ -53,55 +72,46 @@ public class Cycle extends Path {
 			// Limit result count
 			holder += "LIMIT 1\n";
 			
-			
-			
-			/*
-			String temp = "simplecycle";
-			holder += sourceNodeName + "#ml=" + minLength + "%" + temp;
-			holder += "-" + attributeFocus + "%";
-			if(usePassingRestriction){
-				if(toPass) holder += "Pass->";
-				else holder += "NotPass->";
-
-				for(String s : restrictionPathNodes)
-					holder += s + ">";
-			}
-			*/
 			return holder.substring(0, holder.length() - 1);
 		}else{
 			
 			//Set pattern as a Cycle
 			holder += "MATCH path = (src:Place { name:'"
 			+sourceNodeName+ "'})-[:ROAD*1..50]-(src:Place { name:'"+sourceNodeName+"'})\n";
+			//Define restriction nodes
+			if(usePassingRestriction){
+				for(String s : restrictionPathNodes)
+					holder += ",(node"+s+":Place { name:'"+s+"' })\n";
+			}
 			//Choose variables to use
 			holder += "WITH path, src,reduce(" +attributeFocus + "=0, r in relationships(path) | "
 				+ attributeFocus + "+r." + attributeFocus + ") AS total" + attributeFocus + "\n";
+			if(usePassingRestriction){
+				for(String s : restrictionPathNodes)
+					holder += ",node"+s+"\n";
+			}
 			//Force single occurrence of every in-path node for ALL nodes
 			holder += "WHERE ";
 			holder += "ALL(x IN tail(nodes(path)) ";
 			holder += "WHERE SINGLE(y IN tail(nodes(path)) WHERE x=y))\n";
 			//Force minimum value of focused variable
 			holder += "AND total"+attributeFocus+" >= "+minLength+"\n";
+			//Define Restriction Type
+			if(usePassingRestriction){
+				if(toPass)
+					for(String s : restrictionPathNodes)
+						holder +=  "AND node"+s+" IN nodes(path)\n";
+				else
+					for(String s : restrictionPathNodes)
+						holder +=  "AND NONE (n IN nodes(path) WHERE n=node"+s+")\n";
+					
+			}
 			// Return values
 			holder += "RETURN path, total"+attributeFocus+"\n";
 			//Order FIRST by reduction of variable, THEN largest path (in nodes)
 			holder += "ORDER BY total"+attributeFocus+" ASC, length(path) DESC\n";
 			// Limit result count
 			holder += "LIMIT 1\n";
-			
-			
-			/*
-			String temp = "cycle";
-			holder += sourceNodeName + "#ml=" + minLength + "%" + temp;
-			holder += "-" + attributeFocus + "%";
-			if(usePassingRestriction){
-				if(toPass) holder += "Pass->";
-				else holder += "NotPass->";
-
-				for(String s : restrictionPathNodes)
-					holder += s + ">";
-			}
-			*/
 			
 			return holder.substring(0, holder.length() - 1);
 		}
